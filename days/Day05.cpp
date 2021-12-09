3bb80754c7f65337cbc015d76e6b5e5ba57676c9
Will Killian <william.killian@nextsilicon.com>
2021-12-09 09:02:27 -0500

    add branch logic to speed up day 5
    Signed-off-by: Will Killian <william.killian@nextsilicon.com>

--- days/Day05.cpp
+++ days/Day05.cpp
@@ -71,12 +71,32 @@ Day::answer<solve_part2>
 Day::solve(Day::parsed_type const& data, Day::opt_answer) {
   std::array<std::array<int,1'000>,1'000> board { {{0}} };
   int total{0};
+
   for (auto&& [src, dst] : data) {
     if (valid<solve_part2>({src, dst})) {
       day05::point const delta = delta_of(src, dst);
-      for (auto loc = src; loc != dst; loc += delta) {
-        if (++board[loc.y][loc.x] == 2)
-          ++total;
+      if (src.x == 0) {
+        if (src.y != 0) {
+          // vertical
+          for (auto loc = src; loc != dst; loc.y += delta.y) {
+            if (++board[loc.y][loc.x] == 2)
+              ++total;
+          }
+        }
+      } else {
+        if (src.y != 0) {
+          // diagonal
+          for (auto loc = src; loc != dst; loc += delta) {
+            if (++board[loc.y][loc.x] == 2)
+              ++total;
+          }
+        } else {
+          // horizontal
+          for (auto loc = src; loc != dst; loc.x += delta.x) {
+            if (++board[loc.y][loc.x] == 2)
+              ++total;
+          }
+        }
       }
       if (++board[dst.y][dst.x] == 2)
         ++total;
